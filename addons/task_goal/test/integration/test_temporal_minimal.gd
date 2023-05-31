342410c25b6d22905fdc86e8a66646aeef3c8ea7
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-05-30 22:11:19 -0700

    Add a fake table.

--- addons/task_goal/test/integration/test_temporal_minimal.gd
+++ addons/task_goal/test/integration/test_temporal_minimal.gd
@@ -58,30 +58,44 @@ func test_propagate_constraints() -> void:
 	_validate_task_constraints(simple_temporal_network, task_name, start_time, duration)
 
 	for c in simple_temporal_network.constraints:
-		constrains_array.append(c.to_dictionary())
+		var dictionary = c.to_dictionary()
+		constrains_array.append(dictionary)
 	assert_eq_deep(constrains_array, [
 		{ "resource_name": "dummy constraint", "time_interval": Vector2i(0, 25), "duration": 25, "temporal_qualifier": 2},
 		{ "resource_name": "Task 1", "time_interval": Vector2i(5, 15), "duration": 10, "temporal_qualifier": 2}]
 	)
 
+
 func test_propagate_constraints_variation_1() -> void:
+	seed(12345)
 	var simple_temporal_network: SimpleTemporalNetwork = SimpleTemporalNetwork.new()
 	var task_constraints_data = [
-		{"task_name": "Task 2", "duration": 12},
-		{"task_name": "Task 3", "duration": 15},
-		{"task_name": "Task 4", "duration": 8},
+		{"task_name": "Concept Art", "duration": 14, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Game Design Document", "duration": 30, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Character Modeling", "duration": 45, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Level Design", "duration": 60, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "UI Design", "duration": 20, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Sound Effects", "duration": 25, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Music Composition", "duration": 40, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Programming Mechanics", "duration": 70, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "QA Testing", "duration": 50, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
+		{"task_name": "Marketing", "duration": 35, "temporal_qualifier": TemporalConstraint.TemporalQualifier.OVERALL},
 	]
 
 	for data in task_constraints_data:
-		var task_constraints: TemporalConstraint = TemporalConstraint.new(0, 100, data.duration, TemporalConstraint.TemporalQualifier.AT_START, data.task_name)
+		var start_time = randi() % 100
+		var end_time = start_time + data.duration + randi() % 50
+		var task_constraints: TemporalConstraint = TemporalConstraint.new(start_time, end_time, data.duration, data.temporal_qualifier, data.task_name)
 		simple_temporal_network.add_temporal_constraint(task_constraints)
 
 	print("STN after adding all constraints: %s" % str(simple_temporal_network.to_dictionary()))
 
 	var constrains_array = []
 	for c in simple_temporal_network.constraints:
-		constrains_array.append(c.to_dictionary())
+		var dictionary = c.to_dictionary()
+		dictionary.erase("constraints")
+		constrains_array.append(dictionary)
+
 	print("STN constraints after adding task constraints: ", constrains_array)
 
-	assert_eq_deep(constrains_array,  [{ "resource_name": "Task 2", "time_interval": Vector2i(0, 100), "duration": 12, "temporal_qualifier": 0 }, { "resource_name": "Task 3", "time_interval": Vector2i(0, 100), "duration": 15, "temporal_qualifier": 0 }, { "resource_name": "Task 4", "time_interval": Vector2i(0, 100), "duration": 8, "temporal_qualifier": 0 }]) 
-
+	assert_eq_deep(constrains_array, [{ "resource_name": "Concept Art", "time_interval": Vector2i(56, 117), "duration": 14, "temporal_qualifier": 2 }, { "resource_name": "Game Design Document", "time_interval": Vector2i(41, 91), "duration": 30, "temporal_qualifier": 2 }, { "resource_name": "Character Modeling", "time_interval": Vector2i(6, 70), "duration": 45, "temporal_qualifier": 2 }, { "resource_name": "Level Design", "time_interval": Vector2i(41, 139), "duration": 60, "temporal_qualifier": 2 }, { "resource_name": "UI Design", "time_interval": Vector2i(38, 61), "duration": 20, "temporal_qualifier": 2 }, { "resource_name": "Sound Effects", "time_interval": Vector2i(2, 73), "duration": 25, "temporal_qualifier": 2 }, { "resource_name": "Music Composition", "time_interval": Vector2i(36, 112), "duration": 40, "temporal_qualifier": 2 }, { "resource_name": "Programming Mechanics", "time_interval": Vector2i(17, 111), "duration": 70, "temporal_qualifier": 2 }, { "resource_name": "QA Testing", "time_interval": Vector2i(1, 73), "duration": 50, "temporal_qualifier": 2 }, { "resource_name": "Marketing", "time_interval": Vector2i(28, 72), "duration": 35, "temporal_qualifier": 2 }])
