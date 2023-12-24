4e2fdaebbbebc001c15445b7e87e8c26705da0d3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-24 10:17:51 -0800

    Parse sexpressions.

--- addons/task_goal/utility/hddl.gd
+++ addons/task_goal/utility/hddl.gd
@@ -5,96 +5,102 @@ var objects = {}
 var actions = {}
 var methods = {}
 
+
+func parse_sexp(sexp: String) -> Array:
+    var stack = [[]]
+    var word = ""
+    for char in sexp:
+        if char == '(':
+            stack.append([])
+        elif char == ')':
+            if word:
+                stack[-1].append(word.strip())
+                word = ""
+            temp = stack.pop()
+            stack[-1].append(temp)
+        elif char == ' ':
+            if word:
+                stack[-1].append(word.strip())
+                word = ""
+        else:
+            word += char
+    return stack[0]
+
+
 func parse_method_preconditions(method):
-	var lines = method.split("\n")
-	var method_name = ""
-	var parameters = []
-	var task = ""
-	var precondition = ""
-	var ordered_subtasks = []
-	for i in range(lines.size()):
-		var line = lines[i].strip()
-		if line.begins_with("(:method"):
-			method_name = line.replace("(:method ", "").replace(")", "")
-		elif line.begins_with(":parameters"):
-			var params_def = line.replace(":parameters (", "").replace(")", "").split(" ")
-			for j in range(0, params_def.size(), 2):
-				parameters.append({params_def[j]: params_def[j+1]})
-		elif line.begins_with(":task"):
-			task = line.replace(":task (", "").replace(")", "")
-		elif line.begins_with(":precondition"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				precondition += lines[i].strip()
-		elif line.begins_with(":ordered-subtasks"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				ordered_subtasks.append(lines[i].strip())
-	methods[method_name] = {"parameters": parameters, "task": task, "precondition": precondition, "ordered_subtasks": ordered_subtasks}
+    var sexp = parse_sexp(method)
+    var method_name = sexp[1]
+    var parameters = {}
+    var task = ""
+    var precondition = ""
+    var ordered_subtasks = []
+    
+    for i in range(2, sexp.size()):
+        var sublist = sexp[i]
+        if sublist[0] == ":parameters":
+            for j in range(1, sublist.size(), 2):
+                parameters[sublist[j]] = sublist[j+1]
+        elif sublist[0] == ":task":
+            task = sublist[1]
+        elif sublist[0] == ":precondition":
+            precondition = sublist.slice(1, sublist.size())
+        elif sublist[0] == ":ordered-subtasks":
+            ordered_subtasks = sublist.slice(1, sublist.size())
+            
+    methods[method_name] = {"parameters": parameters, "task": task, "precondition": precondition, "ordered_subtasks": ordered_subtasks}
+    
 
 func parse_definition(definition):
-	var lines = definition.split("\n")
-	for i in range(lines.size()):
-		var line = lines[i].strip()
-		if line.begins_with("(:types"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				var type_def = lines[i].strip().split(" - ")
-				types[type_def[0]] = type_def[1]
-		elif line.begins_with("(:objects"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				var object_def = lines[i].strip().split(" - ")
-				objects[object_def[0]] = object_def[1]
-		elif line.begins_with("(:action"):
-			parse_action(line)
-		elif line.begins_with("(:method"):
-			parse_method_preconditions(line)
+    var sexp = parse_sexp(definition)
+    for sublist in sexp:
+        if sublist[0] == ":types":
+            types[sublist[1]] = sublist[2]
+        elif sublist[0] == ":objects":
+            objects[sublist[1]] = sublist[2]
+        elif sublist[0] == ":action":
+            parse_action(sublist)
+        elif sublist[0] == ":method":
+            parse_method_preconditions(sublist)
+
 
 func parse_initialization(initialization):
-	var lines = initialization.split("\n")
-	for i in range(lines.size()):
-		var line = lines[i].strip()
-		if line.begins_with("(:object-instances"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				var instance_def = lines[i].strip().split(" {")
-				var instance_name = instance_def[0]
-				var instance_properties = instance_def[1].replace("}", "").split(", ")
-				var properties = {}
-				for prop in instance_properties:
-					var prop_def = prop.split(" = ")
-					properties[prop_def[0]] = prop_def[1]
-				objects[instance_name] = properties
+    var sexp = parse_sexp(initialization)
+    for sublist in sexp:
+        if sublist[0] == ":object-instances":
+            var instance_name = sublist[1]
+            var properties = {}
+            for i in range(2, len(sublist), 2):
+                properties[sublist[i]] = sublist[i+1]
+            objects[instance_name] = properties
+
 
 func parse_action(action):
-	var lines = action.split("\n")
-	var action_name = ""
-	var parameters = []
-	var duration = 0
-	var precondition = ""
-	var effect = ""
-	for i in range(lines.size()):
-		var line = lines[i].strip()
-		if line.begins_with("(:action"):
-			action_name = line.replace("(:action ", "").replace(")", "")
-		elif line.begins_with(":parameters"):
-			var params_def = line.replace(":parameters (", "").replace(")", "").split(" ")
-			for j in range(0, params_def.size(), 2):
-				parameters.append({params_def[j]: params_def[j+1]})
-		elif line.begins_with(":duration"):
-			duration = int(line.replace(":duration (= ?duration ", "").replace(")", ""))
-		elif line.begins_with(":precondition"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				precondition += lines[i].strip()
-		elif line.begins_with(":effect"):
-			while not lines[i].strip().ends_with(")"):
-				i += 1
-				effect += lines[i].strip()
-	actions[action_name] = {"parameters": parameters, "duration": duration, "precondition": precondition, "effect": effect}
+    var sexp = parse_sexp(action)
+    var action_name = sexp[1]
+    var parameters = {}
+    var duration = 0
+    var precondition = []
+    var effect = []
+    
+    for i in range(2, sexp.size()):
+        var sublist = sexp[i]
+        if sublist[0] == ":parameters":
+            for j in range(1, sublist.size(), 2):
+                parameters[sublist[j]] = sublist[j+1]
+        elif sublist[0] == ":duration":
+            duration = int(sublist[1])
+        elif sublist[0] == ":precondition":
+            for j in range(1, sublist.size()):
+                precondition.append(sublist[j])
+        elif sublist[0] == ":effect":
+            for j in range(1, sublist.size()):
+                effect.append(sublist[j])
+                
+    actions[action_name] = {"parameters": parameters, "duration": duration, "precondition": precondition, "effect": effect}
+
 
 var regex = RegEx.new()
+
 
 func normalize_pddl(pddl: String) -> String:
 	var lines = pddl.split("\n")
