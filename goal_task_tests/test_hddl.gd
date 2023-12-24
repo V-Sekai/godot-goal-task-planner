dc9649d00aabaa15265cf9a1ee7bf1e91fccbb38
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-24 15:03:18 -0800

    Add fixme tests.

--- goal_task_tests/test_hddl.gd
+++ goal_task_tests/test_hddl.gd
@@ -54,22 +54,6 @@ func test_parse_sexp():
 	assert_eq(sexp, expected_output)
 
 
-func test_parse_method_preconditions():
-	var sexp = [[":method", "have_first", ":parameters", ["?x", "?y"], ":task", ["swap", "?x", "?y"], ":precondition", ["and", ["have", "?x"], ["not", ["have", "?y"]]], ":ordered-subtasks", ["and", ["drop", "?x"], ["pickup", "?y"]]]]
-	
-	parser.parse_method_preconditions(sexp)
-	
-	# Check if the key exists
-	if "have_first" in parser.methods:
-		gut.p("Key exists: %s" % parser.methods["have_first"])
-	else:
-		gut.p("Key 'have_first' does not exist in parser.methods")
-	
-	var expected_output = {"parameters": {"?x": "?y"}, "task": "swap", "precondition": [["have", "?x"], ["not", ["have", "?y"]]], "ordered_subtasks": [["drop", "?x"], ["pickup", "?y"]]}
-	
-	assert_ne_deep(parser.get("have_first"), expected_output)
-	
-
 func test_parse_definition():
 	var sexp = [[":types", "a", "b"]]
 	parser.parse_definition(sexp)
@@ -83,16 +67,33 @@ func test_parse_initialization():
 	assert_eq(parser.objects.get("instance1"), expected_output)
 
 
-func test_parse_action():
-	var sexp = [[":action", "pickup", ":parameters", ["?a"], ":precondition", ["not", ["have", "?a"]], ":effect", ["have", "?a"]]]
-	var actions = parser.parse_action(sexp)
-	var expected_output = { "parameters": ["?a"], "precondition": ["have", "?a"], "effect": ["have", "?a"]}
-	assert_ne(actions.get("pickup"), expected_output)
-
-
 func test_normalize_pddl():
 	var pddl = "(define (domain basic) (:requirements :hierarchy :negative-preconditions :method-preconditions))"
 	var normalized_pddl = parser.normalize_pddl(pddl)
 	var expected_output = " ( define ( domain basic ) ( :requirements :hierarchy :negative-preconditions :method-preconditions ) ) "
 	print(normalized_pddl)
 	assert_eq(normalized_pddl, expected_output)
+
+
+func test_parse_method_preconditions_fixme():
+	var sexp = [[":method", "have_first", ":parameters", ["?x", "?y"], ":task", ["swap", "?x", "?y"], ":precondition", ["and", ["have", "?x"], ["not", ["have", "?y"]]], ":ordered-subtasks", ["and", ["drop", "?x"], ["pickup", "?y"]]]]
+	
+	parser.parse_method_preconditions(sexp)
+	
+	# Check if the key exists
+	if "have_first" in parser.methods:
+		gut.p("Key exists: %s" % parser.methods["have_first"])
+	else:
+		gut.p("Key 'have_first' does not exist in parser.methods")
+	
+	var expected_output = {"parameters": {"?x": "?y"}, "task": "swap", "precondition": [["have", "?x"], ["not", ["have", "?y"]]], "ordered_subtasks": [["drop", "?x"], ["pickup", "?y"]]}
+	
+	assert_ne_deep(parser.get("have_first"), expected_output)
+
+
+func test_parse_action_fixme():
+	var sexp = [[":action", "pickup", ":parameters", ["?a"], ":precondition", ["not", ["have", "?a"]], ":effect", ["have", "?a"]]]
+	var actions = parser.parse_action(sexp)
+	var expected_output = { "parameters": ["?a"], "precondition": ["have", "?a"], "effect": ["have", "?a"]}
+	assert_ne(actions.get("pickup"), expected_output)
+
