bc79b943f543316c6dc14b8782b67885cf68607c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-12-24 21:25:17 -0800

    Remove print spam.

--- goal_task_tests/core/test_hddl.gd
+++ goal_task_tests/core/test_hddl.gd
@@ -77,7 +77,6 @@ func parse_sexp(expression: String) -> Array:
 func test_parse_sexp():
 	var sexp = parse_sexp(test_string)
 	var expected_output = [["define", ["domain", "basic"], [":requirements", ":hierarchy", ":negative-preconditions", ":method-preconditions"], [":predicates", ["have", "?a"]], [":task", "swap", ":parameters", ["?x", "?y"]], [":action", "pickup", ":parameters", ["?a"], ":precondition", ["not", ["have", "?a"]], ":effect", ["have", "?a"]], [":action", "drop", ":parameters", ["?a"], ":precondition", ["have", "?a"], ":effect", ["not", ["have", "?a"]]], [":method", "have_first", ":parameters", ["?x", "?y"], ":task", ["swap", "?x", "?y"], ":precondition", ["and", ["have", "?x"], ["not", ["have", "?y"]]], ":ordered-subtasks", ["and", ["drop", "?x"], ["pickup", "?y"]]], [":method", "have_second", ":parameters", ["?x", "?y"], ":task", ["swap", "?x", "?y"], ":precondition", ["and", ["have", "?y"], ["not", ["have", "?x"]]], ":ordered-subtasks", ["and", ["drop", "?y"], ["pickup", "?x"]]]]]
-	print(sexp)
 	assert_eq(sexp, expected_output)
 
 
